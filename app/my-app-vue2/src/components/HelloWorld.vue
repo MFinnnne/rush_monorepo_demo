4c5bcaddfbaf9cce829fa5736d5d0cd7645b522f
MFine <1737374338@qq.com>
2023-03-21 22:04:51 +0800

    add my-utils

--- app/my-app-vue2/src/components/HelloWorld.vue
+++ app/my-app-vue2/src/components/HelloWorld.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="hello">
-      <TestBtn></TestBtn>
+    <TestBtn></TestBtn>
   </div>
 </template>
 
 <script>
-import {TestBtn} from 'vue2-ui'
-
+import { TestBtn } from 'vue2-ui';
+import { add, divide, minus, multiply } from 'my-utils';
 export default {
-    name: "HelloWorld",
-    props: {
-        msg: String
-    },
-    components: {
-        TestBtn
-    }
-}
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    TestBtn
+  },
+  mounted() {
+    console.log(add(1, 1));
+    console.log(divide(1, 1));
+    console.log(minus(1, 1));
+    console.log(multiply(1, 1));
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
