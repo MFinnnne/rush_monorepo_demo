6365d887b486a828fbe4b4734d8a8f196e2bcb9f
MFine <1737374338@qq.com>
2023-04-13 14:36:08 +0800

    init

--- tools/my-utils/dist/types/math/index.d.ts
+++ tools/my-utils/dist/types/math/index.d.ts
@@ -1,4 +1,4 @@
-import { Matrix } from 'mat';
+import { Matrix } from 'my-test-mat';
 export declare function add(a: number, b: number): number;
 export declare function minus(a: number, b: number): number;
 export declare function multiply(a: number, b: number): number;
