2082109a24ae4dd0abff6c94af863201c902fbd0
MFine <lxemyf@gmail.com>
2023-04-14 09:09:31 +0800

    init

--- app/my-app-vue2/src/main.js
+++ app/my-app-vue2/src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import TestBtn from 'vue2-ui'
+import Vue from 'vue';
+import TestBtn from 'vue2-ui';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(TestBtn);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
