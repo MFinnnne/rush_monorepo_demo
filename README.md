959689780fb29bf89466f4341e5065eb537cc34a
MFine <lxemyf@gmail.com>
2023-04-21 11:51:27 +0800

    增加readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # rush_monorepo_demo
 利用rush.js搭建monorepo仓库的示例
+
+## 目录结构
+
+- **ui/**：
+  - vue2-ui：基于vite的vue2简易组件库
+  - vue3-ui：基于vite的vue3简易组件库
+- **tools**
+  - mat：基于roullup构建，ts编写的简易数学库（copy的）
+  - my-utils：基于roullup构建，ts编写的简易库（主要就是示范作用）
+- **doc**：
+  -  docs：基于vitepress的静态网页，主要用来写文档。目前只是初始样子并未做任何修改
+- **app**：
+  - my-app-vue2:  vue2网页示例
+  - my-app-vue3:  vue2网页示例
+- **common**：rushjs的相关配置
+
+> deploy.py 只是进行了docker 镜像构建和推送到远程仓库的作用 并未登录到docker，所以直接使用会失败。登录操作需要集成到ci中使用相关功能确保密码和用户名不会泄露。不能把密码等敏感参数写在脚本里面
+
--- deploy.py
+++ deploy.py
@@ -2,39 +2,47 @@ import json
 import os
 import subprocess
 
-## 配置 docker 地址
-DOCKER_REGISTRY = '192.68.1.24:80'
+TAG_PREFIX = '192.168.1.24'
 
-shouldDeploy = []
-# load file
+curDir = os.getcwd()
+
+print('##############开始部署##############')
+print('当前目录：{},标签前缀：{}'.format(curDir, TAG_PREFIX))
+print('###################################')
+# load rush.json 
 with open('rush.json') as f:
-    pkg = json.load(f)
-    projects = pkg['projects']
-    for project in projects:
-      # print(project)
-      if project["projectFolder"].startswith("app") or project["projectFolder"].startswith("doc"):
-          shouldDeploy.append({'projectName': project["packageName"],'path': project["projectFolder"]})
-
-# 获取当前工作目录
-current_dir = os.getcwd()
-
-for deploy in shouldDeploy:
-    # change directory
-    os.chdir(deploy['path'])
-    # load packages.json
-    with open('package.json') as f:
-      # parse json
-      pkg = json.load(f)
-      # get version
-      version = pkg['version']
-      # get author
-      author = pkg['author']
-      # 获取项目名称
-      name = pkg['name']
-      # 是否存在 dist目录
-      if not os.path.exists('dist'):
-        print('dist not exists')
-      else:
-        # 执行命令行
-        result = subprocess.run(['docker build -t ' +DOCKER_REGISTRY+'/'+author+ name + '/' + version + ' .'], stdout=subprocess.PIPE)
-        print(result.stdout.decode('utf-8'))
+    projects = json.load(f)['projects']
+    for projects in projects:
+        # change directory
+        os.chdir(curDir)
+        os.chdir(projects['projectFolder'])
+        # is exist Dockerfile
+        if not os.path.exists('Dockerfile'):
+            print('项目：{} 不存在Dockerfile文件,因此默认此项目不需要部署'.format(projects['packageName']))
+            continue
+        # load package.json
+        with open('package.json') as f:
+            package = json.load(f)
+            # get image name
+            name = package['name']
+            # get image version
+            version = package['version']
+            # get image tag
+            author = package['author']
+            tag = TAG_PREFIX + '/'+author+'/' + name + ':' + version
+            iamge_name = TAG_PREFIX + '/'+author+'/' + name
+            # build image
+            print('############# 正在构建镜像: {} ###############'.format(tag))
+            result = subprocess.run(['docker','build','-t',tag,'--no-cache','.' ], stdout=subprocess.PIPE)
+            print(result.stdout.decode('utf-8'))
+            print('############# 镜像: {} 构建结束 ###############'.format(tag))
+            print('############# 开始推送镜像: {} ###############'.format(tag))
+            result = subprocess.run(['docker','push',tag], stdout=subprocess.PIPE)
+            print(result.stdout.decode('utf-8'))
+            print('############# 镜像: {} 推送结束 ###############'.format(tag))
+            
+print('##############部署结束##############')
+print('################# 清理tag为none的镜像 ##################')
+result = subprocess.run(['docker','image','prune','-f'], stdout=subprocess.PIPE)
+print(result.stdout.decode('utf-8'))
+print('################# 清理结束 ##################')
